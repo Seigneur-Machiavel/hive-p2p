d0a60ecab5e24d7cca68877bc7409474a454a8c9
Guillaume <gbisiaux.pro@gmail.com>
2025-09-28 21:16:19 +0200

    Remove browser and server packages along with their associated files and scripts. This includes the deletion of package.json files, index.mjs files, and the binary package for the server. Additionally, the publish-all script has been removed as part of the cleanup process.

--- packages/browser/index.mjs
+++
@@ -1,9 +0,0 @@
-import { Node, createNode } from "../../core/node.mjs";
-import { CryptoCodex } from "../../core/crypto-codex.mjs";
-import CONFIG from "../../core/config.mjs";
-
-const HiveP2P = { Node, createNode, CryptoCodex, CONFIG };
-export { Node, createNode, CryptoCodex, CONFIG };
-export default HiveP2P;
-
-if (typeof window !== 'undefined') window.HiveP2P = HiveP2P;
--- packages/server/index.mjs
+++
@@ -1,7 +0,0 @@
-import { Node, createNode, createPublicNode } from "../../core/node.mjs";
-import { CryptoCodex } from "../../core/crypto-codex.mjs";
-import CONFIG from "../../core/config.mjs";
-
-const HiveP2P = { Node, createNode, createPublicNode, CryptoCodex, CONFIG };
-export { Node, createNode, createPublicNode, CryptoCodex, CONFIG };
-export default HiveP2P;
